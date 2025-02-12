4c8df7162f0baa714865500f2c03d54e9de0688c
Igor Konovodov <konovodov@list.ru>
2025-02-12 19:08:47 +0300

    add: slider-swiper

---
+++ slider/swiper/script.js
@@ -0,0 +1,41 @@
+new Swiper(".image-slider", {
+  //Arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevtEl: ".swiper-button-prev",
+  },
+  //Navigation,
+  //Bullets
+  pagination: {
+    el: ".swiper-pagination",
+    // // Bullets
+    // clickable: true,
+    // //Dynamic
+    // dynamicBullets: true,
+    // //Custom
+    // renderBullet: function (index, className) {
+    //   return '<span class=" ' + className + ' ">' + (index + 1) + "</span>";
+    // },
+
+    //Fraction
+    type: "fraction",
+    //custom
+    renderFraction: function (currentClass, totalClass) {
+      return (
+        'Фото <span class=" ' +
+        currentClass +
+        ' "></span>' +
+        " из " +
+        '<span class=" ' +
+        totalClass +
+        ' "></span>'
+      );
+    },
+    //   //Progressbar
+    //   type: "progressbar",
+  },
+  scrollbar: {
+    el: ".swiper-scrollbar",
+    draggable: true,
+  },
+});
